95244aaad024bf63330e26f1007e9194141a1270
Gaël Foppolo <me@gaelfoppolo.com>
2017-04-15 18:52:44 +0200

    add EEGSnapshot & WaveView

---
+++ AnalytiCeM/EEGSnapshot.swift
@@ -0,0 +1,131 @@
+//
+//  EEGSnapshot.swift
+//  AnalytiCeM
+//
+//  Created by Gaël on 15/04/2017.
+//  Copyright © 2017 Polyech. All rights reserved.
+//
+
+public enum EEGType: Int {
+    case eeg
+    
+    case alphaAbsolute
+    case betaAbsolute
+    case deltaAbsolute
+    case thetaAbsolute
+    case gammaAbsolute
+    
+    case alphaRelative
+    case betaRelative
+    case deltaRelative
+    case thetaRelative
+    case gammaRelative
+    
+    case alphaScore
+    case betaScore
+    case deltaScore
+    case thetaScore
+    case gammaScore
+    
+    public init(_ value: IXNMuseDataPacketType.RawValue) {
+        self = EEGType(value)
+    }
+    
+}
+
+public struct EEGSnapshot {
+    
+    private let leftEar: Double?
+    private let leftFront: Double?
+    private let rightFront: Double?
+    private let rightEar: Double?
+    
+    public var value: Double {
+        
+        var sum: Double = 0
+        var count: Double = 0
+        
+        if let leftEar = leftEar {
+            sum += leftEar
+            count += 1
+        }
+        
+        if let leftFront = leftEar {
+            sum += leftFront
+            count += 1
+        }
+        
+        if let rightFront = leftEar {
+            sum += rightFront
+            count += 1
+        }
+        
+        if let rightEar = leftEar {
+            sum += rightEar
+            count += 1
+        }
+        
+        let returnn: Double
+        
+        // si aucune valeur
+        if (count == 0 || sum == 0) {
+            returnn = 0
+        // s'il y a des valeurs
+        } else  {
+            returnn = sum/count
+        }
+        
+        print(returnn)
+        return returnn
+        
+    }
+    
+    public static let allZeros = EEGSnapshot()
+    
+    public init() {
+        leftEar = nil
+        leftFront = nil
+        rightFront = nil
+        rightEar = nil
+    }
+    
+    public init?(data: IXNMuseDataPacket) {
+        
+        // check type
+        let acceptedType = [
+            IXNMuseDataPacketType.eeg,
+            
+            IXNMuseDataPacketType.alphaAbsolute,
+            IXNMuseDataPacketType.betaAbsolute,
+            IXNMuseDataPacketType.deltaAbsolute,
+            IXNMuseDataPacketType.thetaAbsolute,
+            IXNMuseDataPacketType.gammaAbsolute,
+            
+            IXNMuseDataPacketType.alphaRelative,
+            IXNMuseDataPacketType.betaRelative,
+            IXNMuseDataPacketType.deltaRelative,
+            IXNMuseDataPacketType.thetaRelative,
+            IXNMuseDataPacketType.gammaRelative,
+            
+            IXNMuseDataPacketType.alphaScore,
+            IXNMuseDataPacketType.betaScore,
+            IXNMuseDataPacketType.deltaScore,
+            IXNMuseDataPacketType.thetaScore,
+            IXNMuseDataPacketType.gammaScore
+        ]
+        
+        guard acceptedType.contains(data.packetType()) else { return nil }
+    
+        func extractPoint(_ key: Int) -> Double? {
+            let val = data.values()[key].doubleValue
+            return (val.isNaN) ? nil : val
+        }
+        
+        //type = EEGType(data.packetType().rawValue)
+        leftEar = extractPoint(IXNEeg.EEG1.rawValue)
+        leftFront = extractPoint(IXNEeg.EEG2.rawValue)
+        rightFront = extractPoint(IXNEeg.EEG3.rawValue)
+        rightEar = extractPoint(IXNEeg.EEG4.rawValue)
+        
+    }
+}
---
+++ AnalytiCeM/WaveView.swift
@@ -0,0 +1,74 @@
+//
+//  WaveView.swift
+//  AnalytiCeM
+//
+//  Created by Gaël on 15/04/2017.
+//  Copyright © 2017 Polyech. All rights reserved.
+//
+import UIKit
+import QuartzCore
+
+
+private func buildPath(points: [Double], inBounds bounds: CGRect) -> UIBezierPath? {
+    
+    // il y a au moins deux points
+    guard points.count >= 2 else { return nil }
+    
+    let path = UIBezierPath()
+    
+    path.move(to: CGPoint(x: 0.0, y: bounds.height))
+    
+    for (index, point) in points.enumerated() {
+        
+        // position dans l'axe horizontal
+        let xProgress = CGFloat(index) / CGFloat(points.count - 1)
+        
+        let normalizedValue = CGFloat(point)
+        
+        // ajout au chemin
+        path.addLine(to: CGPoint(x: xProgress * bounds.width,
+                                 y: bounds.height * (1.0 - normalizedValue)))
+    }
+    
+    return path
+}
+
+
+open class WaveView: UIView {
+    
+    open var points: [Double] = [] { didSet { setNeedsDisplay() } }
+    
+    fileprivate let wave = CAShapeLayer()
+    
+    open override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        clipsToBounds = true
+
+        tintColorDidChange()
+        
+        wave.fillColor = nil
+        wave.lineWidth = 1.0
+        
+        layer.addSublayer(wave)
+    }
+    
+    open override func tintColorDidChange() {
+        super.tintColorDidChange()
+        
+        wave.strokeColor = self.tintColor.cgColor
+    }
+    
+    open override func draw(_ rect: CGRect) {
+        
+        wave.path = buildPath(points: points, inBounds: bounds)?.cgPath
+        super.draw(rect)
+    }
+    
+    open override func layoutSubviews() {
+        
+        wave.frame = bounds
+        super.layoutSubviews()
+    }
+}
