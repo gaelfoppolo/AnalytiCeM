3037849f70888ac75f82e20e27f54318ed3f8c1d
Gaël Foppolo <me@gaelfoppolo.com>
2017-04-15 18:53:06 +0200

    constraints to clip viewController

--- AnalytiCeM/MainViewController.swift
+++ AnalytiCeM/MainViewController.swift
@@ -16,7 +16,19 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         let simpleController = SimpleController(nibName: "SimpleController", bundle: nil)
+        
+        simpleController.view.translatesAutoresizingMaskIntoConstraints = false
+        
         self.view.addSubview(simpleController.view)
+        
+        let leftSideConstraint = NSLayoutConstraint(item: simpleController.view, attribute: .left, relatedBy: .equal, toItem: view, attribute: .left, multiplier: 1.0, constant: 0.0)
+        let bottomConstraint = NSLayoutConstraint(item: simpleController.view, attribute: .bottom, relatedBy: .equal, toItem: view, attribute: .bottom, multiplier: 1.0, constant: 0.0)
+        let widthConstraint = NSLayoutConstraint(item: simpleController.view, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 1.0, constant: 0.0)
+        let heightConstraint = NSLayoutConstraint(item: simpleController.view, attribute: .height, relatedBy: .equal, toItem: view, attribute: .height, multiplier: 1.0, constant: 0.0)
+        
+        view.addConstraints([leftSideConstraint, bottomConstraint, heightConstraint, widthConstraint])
+        
+        self.view.layoutIfNeeded()
 
     }
 
