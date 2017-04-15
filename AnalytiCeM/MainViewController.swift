284c909b41cf950305414aee4c0c6f8a32c9b220
Gaël Foppolo <me@gaelfoppolo.com>
2017-04-15 11:54:47 +0200

    add MainController

---
+++ AnalytiCeM/MainViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MainViewController.swift
+//  AnalytiCeM
+//
+//  Created by Gaël on 15/04/2017.
+//  Copyright © 2017 Polyech. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let simpleController = SimpleController(nibName: "SimpleController", bundle: nil)
+        self.view.addSubview(simpleController.view)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
