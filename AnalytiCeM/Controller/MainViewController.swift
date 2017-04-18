5c8267858a56b5f451bec8a446c7d0d7146d15dd
Gaël Foppolo <me@gaelfoppolo.com>
2017-04-18 18:10:23 +0200

    refactor some logic

--- AnalytiCeM/Controller/MainViewController.swift
+++ AnalytiCeM/Controller/MainViewController.swift
@@ -8,12 +8,52 @@
 
 import UIKit
 
-class MainViewController: UIViewController, IXNMuseConnectionListener {
+class MainViewController: UIViewController, IXNMuseListener, IXNMuseConnectionListener, IXNLogListener, IXNMuseDataListener {
+    
+    // MARK: - Properties
+    
+    var manager: IXNMuseManagerIos?
+    weak var muse: IXNMuse?
+    
+    var btManager: BluetoothManager!
+    
+    let maxDataPoints: Int = 500
+    var emptyEEGHistory: Array<EEGSnapshot> = Array<EEGSnapshot>()
+    
+    var eegHistory: [EEGSnapshot] = [EEGSnapshot]()
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet var logView: UITextView!
+    @IBOutlet weak var waveView: WaveView!
+
+    // MARK: - View
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Main did load")
+        // get the manager of Muse (singleton)
+        manager = IXNMuseManagerIos.sharedManager()
+        
+        // set the view as delegate
+        manager?.museListener = self
+        
+        // register as log listener
+        IXNLogManager.instance()?.setLogListener(self)
+        
+        // manager of Bluetooth devices
+        btManager = BluetoothManager()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        let dateStr: String = dateFormatter.string(from: Date()) + (".log")
+        print("\(dateStr)")
+        
+        emptyEEGHistory = Array<EEGSnapshot>(repeating: EEGSnapshot.allZeros, count: maxDataPoints)
+        eegHistory = emptyEEGHistory
+        
+        refreshViews()
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -28,6 +68,10 @@ class MainViewController: UIViewController, IXNMuseConnectionListener {
     }
     
     // MARK: - Muse
+    
+    func museListChanged() {
+        //tableView.reloadData()
+    }
     
     func receive(_ packet: IXNMuseConnectionPacket, muse: IXNMuse?) {
         print(muse?.getName())
@@ -50,11 +94,83 @@ class MainViewController: UIViewController, IXNMuseConnectionListener {
         print(state)
     }
     
+    func receive(_ packet: IXNMuseDataPacket?, muse: IXNMuse?) {
+        
+        guard let packet = packet else { return }
+        
+        log(String(format: "%5.2f %5.2f %5.2f %5.2f", CDouble((packet.values()[IXNEeg.EEG1.rawValue])), CDouble((packet.values()[IXNEeg.EEG2.rawValue])), CDouble((packet.values()[IXNEeg.EEG3.rawValue])), CDouble((packet.values()[IXNEeg.EEG4.rawValue]))))
+        
+        // add data if valid
+        let snapshot = EEGSnapshot(data: packet)
+        if let snapshot = snapshot {
+            eegHistory.append(snapshot)
+            // forget surplus points
+            eegHistory.removeSubrange(0 ..< max(0, eegHistory.count - maxDataPoints))
+            refreshViews()
+        }
+        
+        
+        //        if (packet.packetType() == .alphaRelative /*|| packet?.packetType() == .eeg*/) {
+        
+        //        if let type = type {
+        
+        //let eeg1 = (packet.values()[IXNEeg.EEG1.rawValue])
+        //
+        //            log(String(format: "%5.2f %5.2f %5.2f %5.2f", CDouble((packet.values()[IXNEeg.EEG1.rawValue])), CDouble((packet.values()[IXNEeg.EEG2.rawValue])), CDouble((packet.values()[IXNEeg.EEG3.rawValue])), CDouble((packet.values()[IXNEeg.EEG4.rawValue]))))
+        //        }
+        
+    }
+    
+    func receive(_ packet: IXNMuseArtifactPacket, muse: IXNMuse?) {
+        if packet.blink {
+            log("blink detected")
+        }
+        
+        if packet.jawClench {
+            log("jaw clench detected")
+        }
+    }
+    
+    func receiveLog(_ l: IXNLogPacket) {
+        log(String(format: "%@: %llu raw:%d %@", l.tag, l.timestamp, l.raw as CVarArg, l.message))
+    }
+    
+    func connect() {
+        muse?.register(self)
+        muse?.register(self, type: .artifacts)
+        //muse?.register(self, type: .alphaAbsolute)
+        //        muse?.register(self, type: .alphaRelative)
+        //        muse?.register(self, type: .alphaScore)
+        //muse?.unregisterAllListeners()
+        
+        muse?.register(self, type: .betaRelative)
+        
+        muse?.runAsynchronously()
+    }
+    
     // MARK: - Business
     
     func loadSavedMuse() -> String? {
         
         return UserDefaults.standard.string(forKey: "lastMuse")
+    }
+    
+    func log(_ message: String) {
+        print("\(message)")
+        //logLines?.insert(message, at: 0)
+        //DispatchQueue.main.async(execute: {() -> Void in
+        //self.logView.text = self.logLines?.joined(separator: "\n")
+        //            self.logView.text = (self.logLines as NSArray).componentsJoined(by: "\n")
+        //})
+    }
+    
+    func refreshViews() {
+        
+        // recover only the property we want for each snapshot of the history
+        waveView.points = eegHistory.map({
+            return $0.value
+        })
+        
     }
     
 
--- AnalytiCeM/Controller/SettingsViewController.swift
+++ AnalytiCeM/Controller/SettingsViewController.swift
@@ -7,72 +7,41 @@
 //
 
 import UIKit
-import CoreBluetooth
 
-class SettingsViewController: UIViewController, IXNMuseConnectionListener, IXNMuseDataListener, IXNMuseListener, IXNLogListener, UITableViewDelegate, UITableViewDataSource {
+class SettingsViewController: UIViewController, IXNMuseConnectionListener, IXNMuseListener, UITableViewDelegate, UITableViewDataSource {
     
     // MARK: - Properties
     
     var manager: IXNMuseManagerIos?
     weak var muse: IXNMuse?
-    var logLines: [String]?
-    var isLastBlink: Bool = false
-    var btManager: CBCentralManager?
-    var isBtState: Bool = false
     
-    let maxDataPoints: Int = 500
-    var emptyEEGHistory: Array<EEGSnapshot> = Array<EEGSnapshot>()
-    
-    var eegHistory: [EEGSnapshot] = [EEGSnapshot]()
+    var btManager: BluetoothManager!
     
     // MARK: - IBOutlet
     
     @IBOutlet var tableView: UITableView!
-    @IBOutlet var logView: UITextView!
-    @IBOutlet weak var waveView: WaveView!
     
     // MARK: - View
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        UIApplication.shared.isIdleTimerDisabled = true
+        self.navigationItem.title = "Settings"
+        
+        //UIApplication.shared.isIdleTimerDisabled = true
         
         // get the manager of Muse (singleton)
-        if manager == nil {
-            manager = IXNMuseManagerIos.sharedManager()
-        }
+        manager = IXNMuseManagerIos.sharedManager()
         
         // set the view as delegate
         manager?.museListener = self
         
-        // register as log listener
-        IXNLogManager.instance()?.setLogListener(self)
-        
         // manager of Bluetooth devices
-        btManager = CBCentralManager(delegate: self as? CBCentralManagerDelegate, queue: nil, options: nil)
-        
-        // current state of Bluetooth
-        isBtState = false
+        btManager = BluetoothManager()
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        //tableView = UITableView()
-        //logView = UITextView()
         
-        // properties instance
-        // @todo
-        logLines = [String]()
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
-        let dateStr: String = dateFormatter.string(from: Date()) + (".log")
-        print("\(dateStr)")
-        
-        emptyEEGHistory = Array<EEGSnapshot>(repeating: EEGSnapshot.allZeros, count: maxDataPoints)
-        eegHistory = emptyEEGHistory
-        
-        refreshViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -89,8 +58,10 @@ class SettingsViewController: UIViewController, IXNMuseConnectionListener, IXNMu
     }
     
     @IBAction func scan(_ sender: Any) {
-        manager?.startListening()
-        tableView.reloadData()
+        if (btManager.isBluetoothEnabled()) {
+            manager?.startListening()
+            tableView.reloadData()
+        }
     }
     
     @IBAction func stopScan(_ sender: Any) {
@@ -143,16 +114,7 @@ class SettingsViewController: UIViewController, IXNMuseConnectionListener, IXNMu
         }
     }
     
-    // MARK: - CBCentralManager
-    func centralManagerDidUpdateState(_ central: CBCentralManager) {
-        isBtState = (btManager?.state == .poweredOn)
-    }
-    
     // MARK: - Muse
-    
-    func receiveLog(_ l: IXNLogPacket) {
-        log(String(format: "%@: %llu raw:%d %@", l.tag, l.timestamp, l.raw as CVarArg, l.message))
-    }
     
     func museListChanged() {
         tableView.reloadData()
@@ -179,83 +141,18 @@ class SettingsViewController: UIViewController, IXNMuseConnectionListener, IXNMu
     
     func connect() {
         muse?.register(self)
-        muse?.register(self, type: .artifacts)
-        //muse?.register(self, type: .alphaAbsolute)
-        //        muse?.register(self, type: .alphaRelative)
-        //        muse?.register(self, type: .alphaScore)
-        //muse?.unregisterAllListeners()
-        
-        muse?.register(self, type: .betaRelative)
-        
         muse?.runAsynchronously()
-    }
-    
-    func receive(_ packet: IXNMuseDataPacket?, muse: IXNMuse?) {
-        
-        guard let packet = packet else { return }
-        
-        log(String(format: "%5.2f %5.2f %5.2f %5.2f", CDouble((packet.values()[IXNEeg.EEG1.rawValue])), CDouble((packet.values()[IXNEeg.EEG2.rawValue])), CDouble((packet.values()[IXNEeg.EEG3.rawValue])), CDouble((packet.values()[IXNEeg.EEG4.rawValue]))))
-        
-        // add data if valid
-        let snapshot = EEGSnapshot(data: packet)
-        if let snapshot = snapshot {
-            eegHistory.append(snapshot)
-            // forget surplus points
-            eegHistory.removeSubrange(0 ..< max(0, eegHistory.count - maxDataPoints))
-            refreshViews()
-        }
-        
-        
-        //        if (packet.packetType() == .alphaRelative /*|| packet?.packetType() == .eeg*/) {
-        
-        //        if let type = type {
-        
-        //let eeg1 = (packet.values()[IXNEeg.EEG1.rawValue])
-        //
-        //            log(String(format: "%5.2f %5.2f %5.2f %5.2f", CDouble((packet.values()[IXNEeg.EEG1.rawValue])), CDouble((packet.values()[IXNEeg.EEG2.rawValue])), CDouble((packet.values()[IXNEeg.EEG3.rawValue])), CDouble((packet.values()[IXNEeg.EEG4.rawValue]))))
-        //        }
-        
-    }
-    
-    func receive(_ packet: IXNMuseArtifactPacket, muse: IXNMuse?) {
-        if packet.blink && packet.blink != isLastBlink {
-            log("blink detected")
-        }
-        isLastBlink = packet.blink
-        
-        if packet.jawClench {
-            log("jaw clench detected")
-        }
     }
     
     // MARK: - Business
     
     func log(_ message: String) {
         print("\(message)")
-        logLines?.insert(message, at: 0)
-        DispatchQueue.main.async(execute: {() -> Void in
-            self.logView.text = self.logLines?.joined(separator: "\n")
+        //logLines?.insert(message, at: 0)
+        //DispatchQueue.main.async(execute: {() -> Void in
+            //self.logView.text = self.logLines?.joined(separator: "\n")
 //            self.logView.text = (self.logLines as NSArray).componentsJoined(by: "\n")
-        })
-    }
-    
-    func isBluetoothEnabled() -> Bool {
-        return isBtState
-    }
-    
-    func refreshViews() {
-        
-        // recover only the property we want for each snapshot of the history
-        waveView.points = eegHistory.map({
-            return $0.value
-        })
-        
-        //        func extractBand(_ extractValue: (EEGSnapshot) -> Double) -> [Double] {
-        //            return eegHistory.map(extractValue)
-        //        }
-        //
-        //        waveView.points = extractBand { $0.value }
-        
+        //})
     }
     
     func saveMuse(name: String) {
