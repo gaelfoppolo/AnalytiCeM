30f31db99e7a1e81f912df20afa1e18b432bfac5
Gaël Foppolo <me@gaelfoppolo.com>
2017-05-07 22:39:44 +0200

    fix name

--- AnalytiCeM/Data/ConnectivityManager.swift
+++ AnalytiCeM/Data/ConnectivityManager.swift
@@ -3,7 +3,7 @@
 //  AnalytiCeM
 //
 //  Created by Gaël on 07/05/2017.
-//  Copyright © 2017 Polyech. All rights reserved.
+//  Copyright © 2017 Polytech. All rights reserved.
 //
 
 import Foundation
