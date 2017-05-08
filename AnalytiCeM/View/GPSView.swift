b8c75b15886c5a2690febe71c21ab7224f73c006
Gaël Foppolo <me@gaelfoppolo.com>
2017-05-08 16:03:46 +0200

    create GPSView, custom UIView that will display: - the map centered on the location - the name of the locality & country if possible

---
+++ AnalytiCeM/View/GPSView.swift
@@ -0,0 +1,55 @@
+//
+//  GPSView.swift
+//  AnalytiCeM
+//
+//  Created by Gaël on 08/05/2017.
+//  Copyright © 2017 Polytech. All rights reserved.
+//
+
+import UIKit
+
+class GPSView: UIView {
+
+    @IBOutlet weak var test1: UILabel!
+    @IBOutlet weak var test2: UILabel!
+    
+    // MARK: - Initializers
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    // MARK: - Private Helper Methods
+    
+    // Performs the initial setup.
+    private func setupView() {
+        let view = viewFromNibForClass()
+        view.frame = bounds
+        
+        // Auto-layout stuff.
+        view.autoresizingMask = [
+            UIViewAutoresizing.flexibleWidth,
+            UIViewAutoresizing.flexibleHeight
+        ]
+        
+        // Show the view.
+        addSubview(view)
+    }
+    
+    // Loads a XIB file into a view and returns this view.
+    private func viewFromNibForClass() -> UIView {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: String(describing: type(of: self)), bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+
+}
