cf1213821662ee3274440fb382754d426411bf59
Gaël Foppolo <me@gaelfoppolo.com>
2017-05-23 16:45:03 +0200

    add map: calc distance, show route, zoom to fit route in map

--- AnalytiCeM/Controller/DetailViewController.swift
+++ AnalytiCeM/Controller/DetailViewController.swift
@@ -6,15 +6,19 @@
 //  Copyright © 2017 Polytech. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
-class DetailViewController: UIViewController {
+import RealmSwift
+
+class DetailViewController: UIViewController, MKMapViewDelegate {
     
     // MARK: - Properties
     
     var currentSession: Session!
     private let dateFormatter = DateFormatter()
     private let durationFormatter = DateComponentsFormatter()
+    private let distanceFormatter = MKDistanceFormatter()
     
     // MARK: - IBOutlet
     
@@ -24,6 +28,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var activityTypes: UILabel!
     @IBOutlet weak var mentalState: UILabel!
     @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
     
     // MARK: - UIView
     
@@ -37,6 +42,8 @@ class DetailViewController: UIViewController {
         // duration formatter
         durationFormatter.allowedUnits = [.day, .hour, .minute, .second]
         durationFormatter.unitsStyle = .abbreviated
+        
+        distanceFormatter.unitStyle = .abbreviated
         
         setupUI()
         
@@ -81,6 +88,103 @@ class DetailViewController: UIViewController {
         // mental state
         self.mentalState.text = currentSession.activity?.mentalState?.label
         
+        // length
+        self.distance.text = "Computing map.."
+        
+        // map
+        self.mapView.showsCompass = true
+        self.mapView.showsUserLocation = false
+        self.mapView.showsBuildings = true
+        self.mapView.isZoomEnabled = true
+        self.mapView.isScrollEnabled = true
+        self.mapView.isPitchEnabled = true
+        self.mapView.isRotateEnabled = true
+        self.mapView.mapType = .standard
+        self.mapView.delegate = self
+        
+        // pass the object across thread
+        let sessionRef = ThreadSafeReference(to: currentSession)
+        
+        // do the heavy work in background
+        DispatchQueue.global(qos: .background).async {
+            autoreleasepool {
+                
+                // try to resolve
+                let realm = try! Realm()
+                guard let session = realm.resolve(sessionRef) else {
+                    return
+                    // session was deleted
+                }
+                
+                // retrieve coordinates
+                let coordinates: [CLLocationCoordinate2D] = session.data.map { (data) -> CLLocationCoordinate2D in
+                    return data.gps!.coordinate
+                }
+                
+                // calc distance
+                var distance: Double = 0
+                var lastLoc: CLLocation?
+                
+                for coordinate in coordinates {
+                    
+                    // first pass
+                    guard let last = lastLoc else {
+                        lastLoc = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                        continue
+                    }
+                    
+                    let current = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    let dist = last.distance(from: current)
+                    distance += dist
+                    
+                    lastLoc = current
+                    
+                }
+                
+                // build polyline
+                let polyline = MKPolyline(coordinates: coordinates, count: coordinates.count)
+                
+                // lat
+                let lat: [CLLocationDegrees] = coordinates.map({ $0.latitude })
+                let minLat: CLLocationDegrees? = lat.min()
+                let maxLat: CLLocationDegrees? = lat.max()
+                
+                // lon
+                let lon: [CLLocationDegrees] = coordinates.map({ $0.longitude })
+                let minLon: CLLocationDegrees? = lon.min()
+                let maxLon: CLLocationDegrees? = lon.max()
+                
+                // region
+                var locationSpan = MKCoordinateSpan()
+                locationSpan.latitudeDelta = maxLat! - minLat! + 0.001
+                locationSpan.longitudeDelta = maxLon! - minLon! + 0.001
+                
+                var locationCenter = CLLocationCoordinate2D()
+                locationCenter.latitude = (maxLat! + minLat!) / 2
+                locationCenter.longitude = (maxLon! + minLon!) / 2
+                
+                let region = MKCoordinateRegionMake(locationCenter, locationSpan)
+                
+                // display
+                DispatchQueue.main.async {
+                    
+                    self.mapView.setRegion(region, animated: true)
+                    self.mapView.add(polyline)
+                    self.distance.text = self.distanceFormatter.string(fromDistance: distance)
+                }
+                
+            }
+        }
+        
+    }
+    
+    // MARK: MKMapViewDelegate
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let lineRenderer = MKPolylineRenderer(overlay: overlay)
+        lineRenderer.strokeColor = Theme.current.mainColor
+        lineRenderer.lineWidth = 2
+        return lineRenderer
     }
     
 
